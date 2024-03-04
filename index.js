faddcb3ecdec41646e4a54e075c9569f50849c2b
maaikevp <146950822+maaikevp@users.noreply.github.com>
2024-03-04 13:19:44 +0100

    Extremer connection test

--- index.js
+++ index.js
@@ -58,23 +58,23 @@ mongoose.connect(process.env.CONNECTION_URI)
 
 // CORS 
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
-/* rest of code goes here*/
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:56433', 'http://localhost:10000',
-  'https://testingmovie-apionrender.onrender.com', 'https://testingmovie-apionrender.onrender.com/users', 'https://testingmovie-apionrender.onrender.com/login'];
+// /* rest of code goes here*/
+// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:56433', 'http://localhost:10000',
+//   'https://testingmovie-apionrender.onrender.com', 'https://testingmovie-apionrender.onrender.com/users', 'https://testingmovie-apionrender.onrender.com/login'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 
 app.use(morgan("common"));
